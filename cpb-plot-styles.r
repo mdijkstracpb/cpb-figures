452db6b6e488e02aae606fd277e2ee3b218d3dca
mdijkstra-cpb <m.dijkstra.cpb@gmail.com>
2017-03-17 14:46:54 +0100

    Added different plot types

--- cpb-plot-styles.r
+++ cpb-plot-styles.r
@@ -1,29 +1,37 @@
 #
 ## Define constants
 #
+PLOT.TYPE.SCATTER  = "scatter" # for line plot
 PLOT.TYPE.LINE     = "line" # for line plot
 PLOT.TYPE.BARH     = "barh" # for horizontal bar plot
 PLOT.TYPE.BARV     = "barv" # for vertical bar plot
 PLOT.TYPE.BARVLINE = "barvline" # for vertical bar plot with line
-PLOT.TYPES         = c(PLOT.TYPE.LINE, PLOT.TYPE.BARV) # add implemented types here
+PLOT.TYPES         = c(PLOT.TYPE.SCATTER, PLOT.TYPE.LINE, PLOT.TYPE.BARV, PLOT.TYPE.BARH, PLOT.TYPE.BARVLINE) # add implemented types here
 
 
 #
 ## Define all plot styles
 #
 
-# get colors
-cols = get_color_scheme("cpb-color-scheme-original.csv", "cpb_md_light")
-
-# line specs
-line_lwd = 5
-
 # horizontal line at zero
+line_zero_lty = 2
 line_zero_lwd = 2
-line_zero_col = "lightgray"
+line_zero_col = "#F0F0F0"
+
+# axes
+axis_lab_col = 1
 
 # future
 future_col = "skyblue"
+
+# get colors
+cols = get_color_scheme("cpb-color-scheme-original.csv", "cpb_md_light")
+
+# PLOT.TYPE.SCATTER
+symbol = c(1, 3:11)
+
+# PLOT.TYPE.LINE
+line_lwd = 5
 
 # get dimensions TODO: needs to be parameterized...
 pdf_width	= 7.5 # cm
--- cpb-plot.r
+++ cpb-plot.r
@@ -12,7 +12,7 @@ if (.DEBUG)
 	# csvdata = read.csv("data/C17_211_fig_wrldhndel.txt", sep = "\t", header = F, as.is = T) # C17_211_fig_wrldhndel.txt # C17_233_bedr_inves_ex_wo
 	csvdata = read.csv("data/15mrt/C17_357_kader-terugblik.txt", sep = "\t", header = F, as.is = T) # C17_211_fig_wrldhndel.txt # C17_233_bedr_inves_ex_wo
 	arg = NULL
-	arg$type = PLOT.TYPE.LINE#PLOT.TYPE.BARV#PLOT.TYPE.BARV #PLOT.TYPE.LINE
+	arg$type = PLOT.TYPE.BARVLINE#PLOT.TYPE.SCATTER#PLOT.TYPE.BARV #PLOT.TYPE.LINE
 	arg$future = 2016
 	arg$xlab  = "Example x"
 	arg$y1lab = "Example y1"
@@ -39,7 +39,6 @@ x_show      = if (any(is.na(x))) x[which(!is.na(x))] else NULL # if x_show == NU
 x           = fill_x(x)
 x.range		= range(x)
 
-
 #
 ## Open device
 #
@@ -54,21 +53,23 @@ if (.DEBUG)
 
 par(mai = pdf_mai)
 
-if (PLOT.TYPE.LINE == arg$type)
+if (is.element(arg$type, c(PLOT.TYPE.SCATTER, PLOT.TYPE.LINE)))
 {
-	# Set bg + future
+	# Set future
 	plot(NA, NA, axes = F, xlim = x.range, ylim = y.range, xlab = "", ylab = "", t = "n")
 	if (!is.null(arg$future)) rect(arg$future, y.range[1], x.range[2], y.range[2], density = 20, col = future_col, border = NA)
 
 	# horizontal line at zero
-	abline(h = 0, lty = 2, col = line_zero_col, lwd = line_zero_lwd)
+	abline(h = 0, lty = line_zero_lty, col = line_zero_col, lwd = line_zero_lwd)
 
 	par(new = T)
 
 	curves = list()
 	for (i in 1:nc)
 	{
-		plot(x, y[, i], axes = F, xlim = x.range, ylim = y.range, xlab = "", ylab = "", t = "b", lwd = line_lwd, col = cols$fg[i])
+		this_plot_type = if (PLOT.TYPE.SCATTER == arg$type) "p" else "b"
+
+		plot(x, y[, i], axes = F, xlim = x.range, ylim = y.range, xlab = "", ylab = "", t = this_plot_type, lwd = line_lwd, col = cols$fg[i], pch = symbol[i])
 		curves[[1 + length(curves)]] = list(x = x, y = y[, i])
 		par(new = i < nc)
 	}
@@ -89,37 +90,60 @@ if (PLOT.TYPE.LINE == arg$type)
 
 	# add labels
 	#Hmisc::labcurve(curves, labels = colnames(y), col = cols$fg.txt, adj = 0, cex = 1.1, method = "arrow", arrow.factor = 2)
+	if (PLOT.TYPE.SCATTER == arg$type)
+	{
+		legend("bottomright", legend = colnames(y), bg="transparent", bty = "n", col = cols$fg, pch = symbol, text.col = cols$fg, cex = 1)
+	}
+	
 	if (PLOT.TYPE.LINE == arg$type) Hmisc::labcurve(curves, labels = colnames(y), col = cols$fg.txt, cex = 1.1)
+}
+
+if (is.element(arg$type, c(PLOT.TYPE.BARV, PLOT.TYPE.BARH, PLOT.TYPE.BARVLINE)))
+{
+	nc. = nc
+	if (PLOT.TYPE.BARVLINE == arg$type) nc. = nc. - 1
+	x.range = c(1, (1 + nc.) * length(x))
+	rect_future_x0 = 0.5 + (1 + nc.) * which(arg$future == x) - (1 + nc.)
+	rect_future_x1 = 0.5 + (1 + nc.) * length(x)
 }
 
 if (PLOT.TYPE.BARV == arg$type)
 {
-	x.range = c(1, 3 * length(x))
-	rect_future_x0 = 0.5 + 3 * which(arg$future == x)
-	rect_future_x1 = 0.5 + 3 * length(x)
-	
-	#plot(NA, NA, axes = F, xlim = x.range, ylim = y.range, xlab = "", ylab = "", t = "n")
-	#if (!is.null(arg$future)) rect(rect_future_x0, y.range[1], rect_future_x1, y.range[2], density = 20, col = future_col, border = NA)
-	
-	#par(new = T)
 	barplot(NA,   xlim = x.range, ylim = y.range, beside = TRUE, axes = F)
 	if (!is.null(arg$future)) rect(rect_future_x0, y.range[1], rect_future_x1, y.range[2], density = 20, col = future_col, border = NA)
 	barplot(t(y), xlim = x.range, ylim = y.range, beside = TRUE, col = cols$fg[1:ncol(y)], legend = rownames(colnames(y)), axes = F, add = T, legend.text = colnames(y), args.legend = list(x = "bottomright", bg="transparent", bty = "n"))
-
-	#axis(2, tick = F, las = 2)
+	
+	axis(2, las = 2)
 }
 
 if (PLOT.TYPE.BARH == arg$type)
 {
-	barplot(t(y), beside= T, horiz = T, axes = F, las = 2, col = cols$fg[1:ncol(y)])
-	axis(1)
+	barplot(NA, xlim = y.range, ylim = x.range, beside = TRUE, axes = F, horiz = T)
+	if (!is.null(arg$future)) rect(y.range[1], rect_future_x0, y.range[2], rect_future_x1, density = 20, col = future_col, border = NA)
+	barplot(t(y), xlim = y.range, ylim = x.range, beside= T, horiz = T, las = 2, col = cols$fg[1:ncol(y)], legend = rownames(colnames(y)), axes = F, add = T, legend.text = colnames(y), args.legend = list(x = "topleft", bg="transparent", bty = "n"))
+#	axis(1)
+}
+
+if (PLOT.TYPE.BARVLINE == arg$type)
+{
+	y. = y[, -ncol(y)]
+	
+	barplot(NA, xlim = x.range, ylim = y.range, beside = TRUE, axes = F, xpd = FALSE)
+	if (!is.null(arg$future)) rect(rect_future_x0, y.range[1], rect_future_x1, y.range[2], density = 20, col = future_col, border = NA)
+	x.bar = barplot(t(y.), xlim = x.range, ylim = y.range, beside = TRUE, col = cols$fg[1:nc.], axes = F, add = T, xpd = FALSE)
+	
+	lines(x.bar, y[, nc], lwd = line_lwd, col = cols$fg[nc], pch = symbol[nc], type = "b")
+
+	axis(2, las = 2)
+	
+	legend("bottomright", legend = colnames(y), bg="transparent", bty = "n", col = cols$fg, pch = c(rep(15, nc - 1), symbol), text.col = cols$fg)
 }
 
 #
 ## Show labels
 #
-title(xlab = arg$xlab)
-title(ylab = arg$y1lab)
-mtext(arg$y2lab,side=4,line=3)
+title(xlab = arg$xlab, col.lab = axis_lab_col)
+title(ylab = arg$y1lab, col.lab = axis_lab_col)
+mtext(arg$y2lab,side=4,line=3, col.lab = axis_lab_col)
 
 if (!.DEBUG) dev.off()
